0220469272b4900fbf5020685e013a0ef30cbc67
Kirill Sokolov <zzzzz0908@yandex.ua>
2019-08-18 18:59:33 +0300

    Добавьте файлы проекта.

---
+++ CoordinateConverter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace GeodeticFunctions
+{
+    public class CoordinateConverter
+    {
+        Ellipsoid ell;
+        
+        /// <summary>
+        /// Возвращает значение плоской координаты X.
+        /// </summary>
+        /// <param name="B"> Широта точки.</param>
+        /// <param name="L"> Долгота точки.</param>
+        /// <param name="L0"> Долгота осевого меридиана.</param>
+        /// <returns></returns>
+        public double GeodeticToPlaneX(double B, double L, double L0)
+        {
+            double a0 = ell.MeridianArc(0, B);
+
+            double a2 = 1.0 / 2.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Cos(B / 180 * PI);
+
+            double a4 = 1.0 / 24.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 3)
+                * (5 - Pow(Tan(B / 180 * PI), 2) + 9 * Pow(ell.GetEta(B), 2) + 4 * Pow(ell.GetEta(B), 4));
+
+            double a6 = 1.0 / 720.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 5)
+                * (61 - 58 * Pow(Tan(B / 180 * PI), 2) + Pow(Tan(B / 180 * PI), 4) + 270 * Pow(ell.GetEta(B), 2)
+                - 330 * Pow(ell.GetEta(B) * Tan(B / 180 * PI), 2));
+
+            double a8 = 1.0 / 40320.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 7)
+                * (1385 - 3111 * Pow(Tan(B / 180 * PI), 2) + 543 * Pow(Tan(B / 180 * PI), 4) - Pow(Tan(B / 180 * PI), 6));
+
+
+            double l = (L - L0) / 180 * PI;
+
+            double x = a0 + a2 * Pow(l, 2) + a4 * Pow(l, 4) + a6 * Pow(l, 6) + a8 * Pow(l, 8);
+
+            return x;
+        }
+
+
+        /// <summary>
+        /// Возвращает значение плоской координаты Y.
+        /// </summary>
+        /// <param name="B"> Широта точки.</param>
+        /// <param name="L"> Долгота точки.</param>
+        /// <param name="L0"> Долгота осевого меридиана.</param>
+        /// <returns></returns>
+        public double GeodeticToPlaneY(double B, double L, double L0)
+        {
+            double b1 = ell.RadiusParallel(B);
+
+            double b3 = 1.0 / 6.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 3) * (1 - Pow(Tan(B / 180 * PI), 2) + Pow(ell.GetEta(B), 2));
+
+            double b5 = 1.0 / 120.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 5)
+                * (5 - 18 * Pow(Tan(B / 180 * PI), 2) + Pow(Tan(B / 180 * PI), 4) + 14 * Pow(ell.GetEta(B), 2)
+                - 58 * Pow(ell.GetEta(B) * Tan(B / 180 * PI), 2));
+
+            double b7 = 1.0 / 5040.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 7)
+                * (61 - 479 * Pow(Tan(B / 180 * PI), 2) + 179 * Pow(Tan(B / 180 * PI), 4) - Pow(Tan(B / 180 * PI), 6));
+
+            double l = (L - L0) / 180 * PI;
+
+            double y = b1 + b3 * Pow(l, 3) + b5 * Pow(l, 5) + b7 * Pow(l, 7);
+
+            return y;
+        }
+
+
+        /// <summary>
+        /// Преобразует геодезические координаты в плоские.
+        /// </summary>
+        /// <param name="B"> Широта точки.</param>
+        /// <param name="L"> Долгота точки.</param>
+        /// <param name="L0"> Долгота осевого меридиана.</param>
+        /// <param name="x"> Плоская координата X.</param>
+        /// <param name="y"> Плоская координата Y.</param>
+        public void GeodeticToPlane(double B, double L, double L0, out double x, out double y)
+        {
+            double a0 = ell.MeridianArc(0, B);
+
+            double a2 = 1.0 / 2.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Cos(B / 180 * PI);
+
+            double a4 = 1.0 / 24.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 3)
+                * (5 - Pow(Tan(B / 180 * PI), 2) + 9 * Pow(ell.GetEta(B), 2) + 4 * Pow(ell.GetEta(B), 4));
+
+            double a6 = 1.0 / 720.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 5)
+                * (61 - 58 * Pow(Tan(B / 180 * PI), 2) + Pow(Tan(B / 180 * PI), 4) + 270 * Pow(ell.GetEta(B), 2)
+                - 330 * Pow(ell.GetEta(B) * Tan(B / 180 * PI), 2));
+
+            double a8 = 1.0 / 40320.0 * ell.RadiusN(B) * Sin(B / 180 * PI) * Pow(Cos(B / 180 * PI), 7)
+                * (1385 - 3111 * Pow(Tan(B / 180 * PI), 2) + 543 * Pow(Tan(B / 180 * PI), 4) - Pow(Tan(B / 180 * PI), 6));
+
+            double b1 = ell.RadiusParallel(B);
+
+            double b3 = 1.0 / 6.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 3) * (1 - Pow(Tan(B / 180 * PI), 2) + Pow(ell.GetEta(B), 2));
+
+            double b5 = 1.0 / 120.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 5)
+                * (5 - 18 * Pow(Tan(B / 180 * PI), 2) + Pow(Tan(B / 180 * PI), 4) + 14 * Pow(ell.GetEta(B), 2)
+                - 58 * Pow(ell.GetEta(B) * Tan(B / 180 * PI), 2));
+
+            double b7 = 1.0 / 5040.0 * ell.RadiusN(B) * Pow(Cos(B / 180 * PI), 7)
+                * (61 - 479 * Pow(Tan(B / 180 * PI), 2) + 179 * Pow(Tan(B / 180 * PI), 4) - Pow(Tan(B / 180 * PI), 6));
+
+            double l = (L - L0) / 180 * PI;
+
+            x = a0 + a2 * Pow(l, 2) + a4 * Pow(l, 4) + a6 * Pow(l, 6) + a8 * Pow(l, 8);
+
+            y = b1 * l + b3 * Pow(l, 3) + b5 * Pow(l, 5) + b7 * Pow(l, 7);
+        }
+
+
+        /// <summary>
+        /// Вычисляет широту точки по плоским координатам.
+        /// </summary>
+        /// <param name="x"> Координата X.</param>
+        /// <param name="y"> Координата Y.</param>
+        /// <returns></returns>
+        public double PlaneToGeodeticLatitude(double x, double y)
+        {
+            double Bx = ell.LatitudeCalc(x);
+            double Nx = ell.RadiusN(Bx);
+            double t = Tan(Bx / 180 * PI);
+            double eta = ell.GetEta(Bx);
+
+            double A2 = -t * Pow(ell.GetV(Bx), 2) / (2 * Pow(Nx, 2));
+
+            double A4 = -A2 / (12.0 * Pow(Nx, 2)) * (5.0 + 3 * Pow(t, 2) + Pow(eta, 2) - 9.0 * Pow(eta, 2) * Pow(t, 2) - 4 * Pow(eta, 4));
+
+            double A6 = A2 / (360 * Pow(Nx, 4)) * (61.0 + 90 * Pow(t, 2) + 45 * Pow(t, 4)
+                + 46 * Pow(eta, 2) - 252 * Pow(eta * t, 2) - 90 * Pow(eta, 4) * Pow(t, 4));
+
+            double B = Bx / 180 * PI + A2 * Pow(y, 2) + A4 * Pow(y, 4) + A6 * Pow(y, 6);
+
+            return B / PI * 180;
+
+        }
+
+
+
+        /// <summary>
+        /// Вычисляет разность долгот данной точки и осевого меридиана
+        /// </summary>
+        /// <param name="x"> Координата X.</param>
+        /// <param name="y"> Координата Y (неприведённая)</param>
+        /// <returns></returns>
+        public double PlaneToGeodeticLongtitudeDelta(double x, double y)
+        {
+            double Bx = ell.LatitudeCalc(x);
+            double Nx = ell.RadiusN(Bx);
+
+            double P1 = 1.0 / (Nx * Cos(Bx / 180 * PI));
+
+            double P3 = -P1 / (6 * Pow(Nx, 2)) * (1 + 2 * Pow(Tan(Bx / 180 * PI), 2) + Pow(ell.GetEta(Bx), 2));
+
+            double P5 = P1 / (120.0 * Pow(Nx, 4)) * (5.0 + 28.0 * Pow(Tan(Bx / 180 * PI), 2) + 24.0 * Pow(Tan(Bx / 180 * PI), 4)
+                + 6.0 * Pow(ell.GetEta(Bx), 2)
+                + 8.0 * Pow(ell.GetEta(Bx), 2) * Pow(Tan(Bx / 180 * PI), 2));
+
+            double l = P1 * y + P3 * Pow(y, 3) + P5 * Pow(y, 5);
+
+            return l / PI * 180;
+        }
+    }
+}
---
+++ Ellipsoid.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+
+namespace GeodeticFunctions
+{
+    public class Ellipsoid
+    {
+        public Ellipsoid(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double a { get; }
+        public double b { get; }
+        
+
+        //сжатие f
+        public double f => (a - b) / a;
+
+        //Первый эксцентриситет 
+        public double e1 => Sqrt((a * a - b * b)) / a;
+
+        //Второй эксцентриситет
+        public double e2 => Sqrt((a * a - b * b)) / b;
+
+        //Вспомогательна функция эксцентриситета
+        public double GetEta(double B)
+        {
+            return e2 * Cos(B / 180 * PI);
+        }
+
+
+
+        /// <summary>
+        /// Первая сфероидическая функция.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns> 
+        public double GetW(double B)
+        {
+            return Sqrt(1 - Pow(e1 * Sin(B / 180 * PI), 2));
+        }
+
+
+        /// <summary>
+        /// Вторая сфероидическая функция.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns> 
+        public double GetV(double B)
+        {
+            return Sqrt(1 + Pow(e2 * Cos(B / 180 * PI), 2));
+        }
+
+
+        /// <summary>
+        /// Возвращает значение радиуса кривизны меридианного сечения.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns>
+        public double RadiusM(double B)
+        {
+            return a * (1 - e1 * e1) / Pow(GetW(B), 3);
+        }
+
+
+        /// <summary>
+        /// Возвращает значение радиуса кривизны сечения первого вертикала.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns>
+        public double RadiusN(double B)
+        {
+            return a / GetW(B);
+        }
+
+
+        /// <summary>
+        /// Возвращает значение среднего радиуса кривизны.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns>
+        public double RadiusR(double B)
+        {
+            return b / Pow(GetW(B), 2);
+        }
+
+
+        /// <summary>
+        /// Возвращает радиус кривизны параллели.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <returns></returns>
+        public double RadiusParallel(double B)
+        {
+            return RadiusN(B) * Cos(B / 180 * PI);
+        }
+
+
+        /// <summary>
+        /// Возвращает радиус кривизны произвольного нормального сечения.
+        /// </summary>
+        /// <param name="B"> Широта точки в градусах.</param>
+        /// <param name="A"> Азимут сечения в градусах.</param>
+        /// <returns></returns>
+        public double RadiusAzimut(double B, double A)
+        {
+            return RadiusN(B) * Cos(B / 180 * PI);
+        }
+
+
+        /// <summary>
+        /// Возвращает значение длины дуги параллели.
+        /// </summary>
+        /// <param name="B"> Широта параллели.</param>
+        /// <param name="L1"> Долгота начальной точки.</param>
+        /// <param name="L2"> Долгота Конечной точки</param>
+        /// <returns></returns>
+        public double ParallelArc(double B, double L1, double L2)
+        {
+            return RadiusParallel(B) * Abs(L2 - L1) / 180 * PI;
+        }
+
+
+        /// <summary>
+        /// Возвращает значение длины дуги меридиана.
+        /// </summary>
+        /// <param name="B1"> Широта начальной точки.</param>
+        /// <param name="B2"> Широта конечной точки.</param>
+        /// <returns></returns>
+        public double MeridianArc(double B1, double B2)
+        {
+            double a0 = 1.0 + 3.0 / 4.0 * Pow(e1, 2) + 45.0 / 64.0 * Pow(e1, 4) + 175.0 / 256.0 * Pow(e1, 6) + 11025.0 / 16384.0 * Pow(e1, 8);
+            double a2 = 3.0 / 4.0 * Pow(e1, 2) + 15.0 / 16.0 * Pow(e1, 4) + 525.0 / 512.0 * Pow(e1, 6) + 2205.0 / 2048.0 * Pow(e1, 8);
+            double a4 = 15.0 / 64.0 * Pow(e1, 4) + 105.0 / 256.0 * Pow(e1, 6) + 2205.0 / 4096.0 * Pow(e1, 8);
+            double a6 = 35.0 / 512.0 * Pow(e1, 6) + 315.0 / 4096.0 * Pow(e1, 8);
+
+            double X1 = a * (1 - e1 * e1) * (a0 * B1 / 180 * PI - a2 / 2 * Sin(2 * B1 / 180 * PI) + a4 / 4 * Sin(4 * B1 / 180 * PI) - a6 / 6 * Sin(6 * B1 / 180 * PI));
+            double X2 = a * (1 - e1 * e1) * (a0 * B2 / 180 * PI - a2 / 2 * Sin(2 * B2 / 180 * PI) + a4 / 4 * Sin(4 * B2 / 180 * PI) - a6 / 6 * Sin(6 * B2 / 180 * PI));
+
+            double X = Abs(X2 - X1);
+            return X;
+        }
+
+
+        /// <summary>
+        /// Вычисляет широту по заданной длине дуги меридиана.
+        /// </summary>
+        /// <param name="X"> Длина меридиана, м.</param>
+        /// <returns></returns>
+        public double LatitudeCalc(double X)
+        {
+            double a0 = 1.0 + 3.0 / 4.0 * Pow(e1, 2) + 45.0 / 64.0 * Pow(e1, 4) + 175.0 / 256.0 * Pow(e1, 6) + 11025.0 / 16384.0 * Pow(e1, 8);
+            double a2 = 3.0 / 4.0 * Pow(e1, 2) + 15.0 / 16.0 * Pow(e1, 4) + 525.0 / 512.0 * Pow(e1, 6) + 2205.0 / 2048.0 * Pow(e1, 8);
+            double a4 = 15.0 / 64.0 * Pow(e1, 4) + 105.0 / 256.0 * Pow(e1, 6) + 2205.0 / 4096.0 * Pow(e1, 8);
+            double a6 = 35.0 / 512.0 * Pow(e1, 6) + 315.0 / 4096.0 * Pow(e1, 8);
+
+            double B = 0;
+            double Bx;
+            double epsilon;
+            do
+            {
+                Bx = 1.0 / a0 * (X / (a * (1 - e1 * e1)) + a2 / 2.0 * Sin(2 * B) - a4 / 4.0 * Sin(4 * B) + a6 / 6.0 * Sin(6 * B));
+                epsilon = Abs(Bx - B);
+                B = Bx;
+            } while (epsilon > 0.00005 / 206265);
+
+            return B * 180 / PI;
+        }
+
+
+
+        /// <summary>
+        /// Возвращает значение площади съемочной трапеции.
+        /// </summary>
+        /// <param name="B1"> Широта южной границы.</param>
+        /// <param name="B2"> Широта северной границы.</param>
+        /// <param name="L1"> Долгота западной границы.</param>
+        /// <param name="L2"> Долгота восточной границы.</param>
+        /// <returns></returns>
+        public double AreaCalculate(double B1, double B2, double L1, double L2)
+        {
+            return b * b * Abs(L2 - L1) / 180.0 * PI * (Sin(B2 / 180 * PI) - Sin(B1 / 180 * PI)
+                + 2.0 / 3.0 * Pow(e1, 2) * (Pow(Sin(B2 / 180 * PI), 3) - Pow(Sin(B1 / 180 * PI), 3))
+                + 3.0 / 5.0 * Pow(e1, 4) * (Pow(Sin(B2 / 180 * PI), 5) - Pow(Sin(B1 / 180 * PI), 5))
+                + 4.0 / 7.0 * Pow(e1, 6) * (Pow(Sin(B2 / 180 * PI), 7) - Pow(Sin(B1 / 180 * PI), 7)));
+
+        }
+
+
+        
+
+
+
+    }
+}
